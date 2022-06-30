ba21bea824879674dc02ff81d60f0cf6c7bd9907
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-06-30 18:21:16 -0300

    Los campos para postear un nuevo juego, ahora estan validados con express-validator

--- api/src/routes/games.js
+++ api/src/routes/games.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {check} = require('express-validator');
 const {getGames, postGame, getGamesByName} = require('./functions');
-
+const {validateFields} = require('../middlewares/validateFields');
 
 router.get('/', async (req, res) => {
     const {name} = req.query;
@@ -18,7 +19,23 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
+//     const {name, description, genres, platforms} = req.body;
+//     try {
+//         const newGame = await postGame(name, description, genres, platforms);
+//         res.json(newGame);
+//     } catch (error) {
+//         res.status(400).send({error: error.message});
+//     }
+// })
+
+router.post('/', [
+    check('name', 'El nombre del juego es obligatorio').not().isEmpty(),
+    check('description', 'La descripcion del juego es obligatoria').not().isEmpty(),
+    check('genres', 'El juego debe tener al menos un género').not().isEmpty(),
+    check('platforms', 'El juego debe tener al menos una plataforma').not().isEmpty(),
+
+],validateFields, async (req, res) => {
     const {name, description, genres, platforms} = req.body;
     try {
         const newGame = await postGame(name, description, genres, platforms);
