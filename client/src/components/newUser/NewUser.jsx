84683406524e99093bc8c5a65115ab652d68cce1
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-07-28 15:48:51 -0300

    estilos al formulario de signin y detalles en el formulario de login

--- client/src/components/newUser/NewUser.jsx
+++ client/src/components/newUser/NewUser.jsx
@@ -3,11 +3,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { CREATE_USER } from '../../redux/actions'
 import validate from './validate'
+import '../../scss/_signin.scss'
 
 export default function NewUser() {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const {newUserResponse} = useSelector(state => state.reducer)
+    const { newUserResponse } = useSelector(state => state.reducer)
     const [formValues, setFormValues] = useState({
         name: '',
         email: '',
@@ -26,8 +27,8 @@ export default function NewUser() {
         })
         setErrors(err)
     }
-    
-    
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (Object.keys(errors).length === 0) {
@@ -40,40 +41,47 @@ export default function NewUser() {
             confirmPassword: ''
         })
     }
-    useEffect(()=>{
-        if(newUserResponse.ok){
+    useEffect(() => {
+        if (newUserResponse.ok) {
             navigate('/login')
-        }else{
+        } else {
             setErrors({
                 ...errors,
-                errorUser : newUserResponse.msg
+                errorUser: newUserResponse.msg
             })
         }
     }, [newUserResponse, navigate]) // eslint-disable-line react-hooks/exhaustive-deps
     return (
-        <form onSubmit={handleSubmit}>
-            {errors.errorUser && <p>{errors.errorUser}</p>}
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" name="name" value={formValues.name} onChange={handleChange}  autoComplete="off"/>
-                {errors.name && <small>{errors.name}</small>}
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" name="email" value={formValues.email} onChange={handleChange}  autoComplete="off"/>
-                {errors.email && <small>{errors.email}</small>}
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" name="password" value={formValues.password} onChange={handleChange} />
-                {errors.password && <small>{errors.password}</small>}
-            </div>
-            <div className="form-group">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={formValues.confirmPassword} onChange={handleChange} />
-                {errors.confirmPassword && <small>{errors.confirmPassword}</small>}
-            </div>
-            <button type='submit'>Create Account</button>
-        </form>
+        <div className="signin">
+            <form className='form-signin__container' onSubmit={handleSubmit}>
+                <div className='signin-title'>
+                    <h1>Create your account</h1>
+                </div>
+                <div className='form-group__container'>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" value={formValues.name} onChange={handleChange} autoComplete="off" />
+                        {errors.name && <smal className="form-msg">{errors.name}</smal>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" className="form-control" id="email" name="email" value={formValues.email} onChange={handleChange} autoComplete="off" />
+                        {errors.email && <sma className="form-msg">{errors.email}</sma>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" id="password" name="password" value={formValues.password} onChange={handleChange} />
+                        {errors.password && <smal className="form-msg">{errors.password}</smal>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={formValues.confirmPassword} onChange={handleChange} />
+                        {errors.confirmPassword && <small className="form-msg">{errors.confirmPassword}</small>}
+                    </div>
+                </div>
+                <button className='signin-btn' type='submit'>Create Account</button>
+                {errors.errorUser && <p>{errors.errorUser}</p>}
+            </form>
+        </div>
     )
 }
