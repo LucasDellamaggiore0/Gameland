e4386f50221ecc814b6553427e47f038350d2c9b
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-08-08 16:44:04 -0300

    modificacion en el get games

--- client/src/components/games/GamesContainer.jsx
+++ client/src/components/games/GamesContainer.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { GET_GAMES } from '../../redux/actions'
 import GamesMap from './GamesMap';
 import Orders from '../orders/Orders';
 
-export default function GamesContainer() {
-    const dispatch = useDispatch();
-    const {games} = useSelector(state => state.reducer);
-    
-    useEffect(() => {
-        dispatch(GET_GAMES());
-    }, [dispatch]);
+export default function GamesContainer(props) {
     return (
         <div className='games--home__container'>
             <section className='games-orders__container'>
                 <h1>Currently trending games</h1>
-                <Orders games={games}/>
+                <Orders games={props.games}/>
             </section>
-            <GamesMap games={games} />
+            <GamesMap games={props.games} />
         </div>
     )
 }
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 	const { games, loading } = useSelector(state => state.reducer);
 	useEffect(() => {
 		dispatch(GET_GAMES());
-	} , [games]);
+	} , [dispatch]);
 	if(loading) {
 		return <Loader />
 	}else{
